7d50fb7dcd6f24066b0b936006d6916e97194cc6
Eros Niko Alvarez <iamerosniko@yahoo.com>
2017-09-27 10:31:21 -0400

    ini

---
+++ MicroAPI/MainController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroAPI
+{
+    public class MainController
+    {
+        private string _URI = "";
+        public MainController(string URI,string Controller)
+        {
+            this._URI = URI+"/"+Controller;
+        }
+
+        public string get()
+        {
+            return "";
+        }
+        public string get(string id)
+        {
+            return "";
+        }
+        public string post(string content)
+        {
+            return "";
+        }
+        public string put(string content,string id)
+        {
+            return "";
+        }
+        public string delete(string id)
+        {
+            return "";
+        }
+    }
+}
